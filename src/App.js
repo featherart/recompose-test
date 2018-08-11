d1a567aec94ade2ae7d2a89ce18ed4f45ecb60ff
Feather Knee <featherknee@gmail.com>
2018-08-10 23:19:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Counter from './Counter'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="App-header">
+          <h1 className="App-title">Welcome to Stuff I Learned</h1>
+        </header>
+        <div>
+          <Counter />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
---
+++ src/Counter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { compose, withState, withHandlers } from 'recompose'
+
+type Props = {
+  count: number,
+  incrementCount: Function,
+  handleIncrement: Function,
+  handleDecrement: Function
+}
+
+const Counter = ({ count, incrementCount, handleIncrement, handleDecrement }: Props) => (
+  <div className="counter-container">
+    <span className='count'>{count}</span>
+    <button type='submit' className='increment-button' onClick={handleIncrement}>Increment</button>
+    <button type='submit' className='decrement-button' onClick={handleDecrement}>Decrement</button>
+  </div>
+)
+
+export default compose(
+  withState('count', 'incrementCount', 0),
+  withHandlers({
+    handleIncrement: ({ incrementCount, count }) => () => {
+      incrementCount(count + 1)
+    },
+    handleDecrement: ({ incrementCount, count }) => () => {
+      incrementCount(count - 1)
+    }
+  })
+)(Counter)
